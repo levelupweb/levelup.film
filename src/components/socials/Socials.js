9ed24da15022260a6cc62127f556cc3934243210
kv9991 <exide-pra@yandex.ru>
2017-09-09 18:13:57 +0300

    fix social links

--- src/components/socials/Socials.js
+++ src/components/socials/Socials.js
@@ -4,23 +4,18 @@ import './socials.css'
 const Socials = () => 
 	<div className="socials">
 		<div className="social vk">
-			<a href="https://vk.com/levelupweb" target="_blank">
+			<a href="https://vk.com/levelupfilm" target="_blank">
 				<i className="fa fa-vk"></i>
 			</a>
 		</div>
-		<div className="social twitter">
-			<a href="https://twitter.com/levelup_web" target="_blank">
-				<i className="fa fa-twitter"></i>
+		<div className="social instagram">
+			<a href="https://www.instagram.com/levelup.film/" target="_blank">
+				<i className="fa fa-instagram"></i>
 			</a>
 		</div>
 		<div className="social facebook">
-			<a href="https://github.com/levelupweb/" target="_blank">
+			<a href="https://www.facebook.com/lvlpfilm/" target="_blank">
 				<i className="fa fa-facebook"></i>
-			</a>
-		</div>
-		<div className="social youtube">
-			<a href="https://t.me/levelupweb" target="_blank">
-				<i className="fa fa-youtube"></i>
 			</a>
 		</div>
 	</div>
