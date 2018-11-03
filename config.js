eba10a6375a84c0defb96ffa1369647f9207b1e0
kv9991 <beatzhitta@gmail.com>
2018-11-03 21:09:03 +0300

    some changes

--- config.js
+++ config.js
@@ -1,24 +1,21 @@
-const dev = process.env.DEV === 'true';
-const ssl = process.env.SSL === 'true';
+const dev = process.env.NODE_ENV === "development";
 const port = process.env.PORT;
-const type = ssl ? 'https://' : 'http://';
-const domain = type + (dev ? (`localhost:${port}`) : process.env.URL);
-const staticfolder = '/dist';
+const domain = "http://" + (dev ? `localhost:${port}` : process.env.URL);
+const staticfolder = "/dist";
 const staticaddr = domain + staticfolder;
-const host = dev ? '127.0.0.1' : process.env.IP;
-const hosturl = type + host;
+const host = dev ? "127.0.0.1" : process.env.IP;
+const hosturl = "http://" + host;
 const sender = process.env.SENDER;
 const receiever = process.env.RECEIEVER;
 const smtpuser = process.env.SMTP_LOGIN;
 const smtppassword = process.env.SMTP_PASSWORD;
 const emailurl = `${domain}/send`;
 const smtphost = process.env.SMTP_HOST;
-const contactposturl = '/email/contact';
-const partnerposturl = '/email/partner';
+const contactposturl = "/email/contact";
+const partnerposturl = "/email/partner";
 
 module.exports.server = {
   dev,
-  ssl,
   port,
   domain,
   static: staticfolder,
@@ -30,19 +27,17 @@ module.exports.server = {
   partnerposturl,
   mail: {
     sender,
-    receiever,
-  },
+    receiever
+  }
 };
 
 module.exports.client = {
   dev,
-  ssl,
   port,
   emailurl,
   contactposturl,
   partnerposturl,
   domain,
   static: staticaddr,
-  hosturl,
+  hosturl
 };
-
